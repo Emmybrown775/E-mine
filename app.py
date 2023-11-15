bd7493e605f668e932423d7c077b1420d74c76c1
Emmybrown775 <elumezeemma@gmail.com>
2023-11-15 09:22:14 +0100

    e-mine

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from why_chose_data import why_choose_us
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template("index.html", why_choose_us=why_choose_us)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ why_chose_data.py
@@ -0,0 +1,31 @@
+why_choose_us = [
+    {
+        "title": "Lucrative Rewards",
+        "content": "At e-Mine, we believe in recognizing your efforts. Enjoy generous rewards for every successful"
+                   " referral and watch your earnings soar."
+
+    },
+    {
+        "title": "Secure and Reliable",
+        "content": "Your trust is our top priority. Rest assured, our platform employs state-of-the-art security "
+                   "measures to safeguard your data and transactions."
+
+    },
+    {
+        "title": "User Friendly Interface",
+        "content": "With our intuitive interface, getting started is a breeze. In just a few clicks, you'll be on your"
+                   " way to reaping the rewards of affiliate marketing"
+    },
+    {
+        "title": "Endless Possibilities for non -referrals",
+        "content": "We believe in creating opportunities for everyone, regardless of their affiliation status. "
+                   "Our platform is designed to achieve financial freedom through the power of Non-affiliate."
+                   " You no longer need to be tied to refer before you reap the benefits of e-Mine; we've opened the "
+                   "door to endless possibilities."
+    },
+    {
+        "title": "Earning Structure",
+        "content": "Our earning structure for referrals and non referral is a structure you "
+                   "have never come across with our VTU system,e-Games,e-contest,e-stores e.t.c"
+    }
+]

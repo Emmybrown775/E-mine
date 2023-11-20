c621bdd7b533e433790bf69e09705550388e05b3
Emmybrown775 <elumezeemma@gmail.com>
2023-11-20 19:32:48 +0100

    about us, how it works and faq implemented

--- app.py
+++ app.py
@@ -22,6 +22,18 @@ def top_earners():
     apps_top_earners = app_top_earners.get_top_earners()
     return render_template("top_earners.html", top_earners=apps_top_earners)
 
+@app.route('/about')
+def about_us():
+    return render_template("about_us.html")
+
+@app.route('/how-it-works')
+def how_it_works():
+    return render_template('how_it_works.html')
+
+
+@app.route('/faq')
+def faq():
+    return render_template("faq.html")
 
 if __name__ == '__main__':
     app.run()
